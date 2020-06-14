c712e0bbb9e3fbf98e70abb61f2b40a679b799fb
panzershaft <contractor.soham@gmail.com>
2020-06-14 23:26:50 +0100

    mend

--- src/Kata/WhoLikesIt.java
+++ src/Kata/WhoLikesIt.java
@@ -1,9 +1,3 @@
-package Kata;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-
 /*
 You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
@@ -14,6 +8,12 @@ likes {"Jacob", "Alex"} // must be "Jacob and Alex like this"
 likes {"Max", "John", "Mark"} // must be "Max, John and Mark like this"
 likes {"Alex", "Jacob", "Mark", "Max"} // must be "Alex, Jacob and 2 others like this"
  */
+
+package Kata;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 
 public class WhoLikesIt {
 	
